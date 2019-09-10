08765e0c3ec2a7dafb9a43fe63804c815625cf51
unknown <df.perezc@uniandes.edu.co>
2019-09-10 09:20:48 -0500

    commit1df

--- proyecto/src/caso1javaProy/Buffer.java
+++ proyecto/src/caso1javaProy/Buffer.java
@@ -1,5 +1,34 @@
 package caso1javaProy;
 
-public class Buffer {
+import java.util.ArrayList;
 
+public class Buffer {
+	
+	//-------------------------------------------AB
+	private int size;
+	private ArrayList<Mensaje> mess;
+	private int numbCli;
+	//-------------------------------------------AE
+
+	public Buffer(int size, int numbCli)
+	{
+		
+	}
+	
+	//-------------------------------------------SGB
+	
+	//-------------------------------------------SGE
+	
+	public synchronized void keepMessage(Mensaje mess)
+	{
+		
+	}
+	
+	public synchronized Mensaje removeMessage()
+	{
+		return new Mensaje();
+	}
+	
+	
+	
 }
---
+++ proyecto/src/caso1javaProy/Main.java
@@ -0,0 +1,10 @@
+package caso1javaProy;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
